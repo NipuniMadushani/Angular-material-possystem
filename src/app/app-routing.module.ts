0786a15666ae62c5787d026fe9edb453ab01d951
NipuniGitAccount <nipunimadushani52@gmail.com>
2019-06-18 17:31:06 +0530

    add lazyloading to route components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './components/dashboard/dashboard.module#DashboardModule'
+  },
+  {
+    path: 'customer',
+    loadChildren: './components/customer/customer.module#CustomerModule'
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+
+  }
 ];
 
 @NgModule({
